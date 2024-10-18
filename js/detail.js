893bcb70c2066621b4d2f951e468f7553d234412
NIRUP <dumpkits@gmail.com>
2024-10-18 10:34:37 +0530

    to master

--- js/detail.js
+++ js/detail.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $.ajax({
         url: '../php/sample.php',
         method: 'GET',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             if (data && Array.isArray(data)) {
-                data.forEach(function(item) {
+                data.forEach(function (item) {
                     var card = $('#seperation-card-template').clone();
                     card.removeAttr('id');
                     card.addClass('card');
                     card.show();
                     card.find('.card_details').html('<strong>Name:</strong> ' + item.name + '<br><strong>Location:</strong> ' + item.location);
-                    card.on('click', function() {
+                    card.on('click', function () {
                         window.location.href = 'details.html?name=' + encodeURIComponent(item.name);
                     });
                     $('#seperation-centers-cards').append(card);
                 });
             }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.error('Failed to fetch data: ' + textStatus + ' - ' + errorThrown);
         }
     });
@@ -26,23 +26,23 @@ $(document).ready(function() {
         url: '../php/endclients.php',
         method: 'GET',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             if (data && Array.isArray(data)) {
-                data.forEach(function(item) {
+                data.forEach(function (item) {
                     var card = $('#end-client-card-template').clone();
                     card.removeAttr('id');
                     card.addClass('card');
                     card.show();
-                    
+
                     card.find('.card_details').html('<strong>Name:</strong> ' + item.name + '<br><strong>Location:</strong> ' + item.location);
-                    card.on('click', function() {
+                    card.on('click', function () {
                         window.location.href = 'details.html?name=' + encodeURIComponent(item.name);
                     });
                     $('#end-clients-cards').append(card);
                 });
             }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.error('Failed to fetch data: ' + textStatus + ' - ' + errorThrown);
         }
     });
@@ -51,24 +51,24 @@ $(document).ready(function() {
         url: '../php/collectioncenters.php',
         method: 'GET',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             if (data && Array.isArray(data)) {
                 console.log(data);
-                data.forEach(function(item) {
+                data.forEach(function (item) {
                     var card = $('#Collection-card-template').clone();
                     card.removeAttr('id');
                     card.addClass('card');
                     card.show();
-                    
-                    card.find('.card_details').html('<strong>Location:</strong> '+ item.location);
-                    card.on('click', function() {
-                        window.location.href = '../UserDetails.html?location=' + encodeURIComponent(item.location)+'&id='+encodeURIComponent(item.id);
+
+                    card.find('.card_details').html('<strong>Location:</strong> ' + item.location);
+                    card.on('click', function () {
+                        window.location.href = '../UserDetails.html?location=' + encodeURIComponent(item.location) + '&id=' + encodeURIComponent(item.id);
                     });
                     $('#Collection-centers-cards').append(card);
                 });
             }
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.error('Failed to fetch data: ' + textStatus + ' - ' + errorThrown);
         }
     });
@@ -80,18 +80,32 @@ $(document).ready(function() {
     $.ajax({
         url: '../php/getSellCount.php',
         method: 'POST',
-        data : {id : id},
-        success : function(data)
-        {
+        data: { id: id },
+        success: function (data) {
             console.log(data.count);
-            if(data.status == true)
+            if (data.status == true)
                 document.getElementById('sellCount').innerHTML = data.count;
             else
-               console.log('failed');
+                console.log('failed');
         },
-        error: function()
-        {
+        error: function () {
             console.log("hello");
         },
-});
+    });
+
+    $.ajax({
+        url: '../php/getBuyCount.php',
+        method: 'POST',
+        data: { id: id },
+        success: function (data) {
+            console.log(data.count);
+            if (data.status == true)
+                document.getElementById('buyCount').innerHTML = data.count;
+            else
+                console.log('failed');
+        },
+        error: function () {
+            console.log("hello");
+        },
+    });
 });
