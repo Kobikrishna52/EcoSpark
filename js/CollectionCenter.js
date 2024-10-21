95c2fb9270594e585589ecaa223e98002c958ee8
NIRUP <dumpkits@gmail.com>
2024-10-21 11:27:59 +0530

    new logo add and index page

---
+++ js/CollectionCenter.js
@@ -0,0 +1,18 @@
+$.ajax({
+    method: 'POST',
+    data: { id: id },
+    url: '../php/getRequestsforBuy.php',
+    success: function (data) {
+        console.log(data);
+        data.forEach((item) => {
+            creatediv(item.id);
+        });
+    }
+});
+function creatediv(id) {
+    console.log("id = " + id);
+    div = document.createElement('div');
+    div.classList.add('inside-content');
+    div.innerHTML = id;
+    buy_requests.appendChild(div);
+}
